29970258e4df36c2d2786fd46f1b6ea9057d1bb6
Yaron Levy <ron5054@gmail.com>
2024-06-15 21:16:32 +0300

    redeploy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = createServer(app)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve('public')))
-}else {
+} else {
     const corsOptions = {
       origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
       credentials: true,
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
   }
 
 
-app.get('/', (req, res) => {
+app.get('/message', (req, res) => {
   res.send('Hello from Server!')
 })
 
