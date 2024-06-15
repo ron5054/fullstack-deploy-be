c060b70b1b84595f7824d9d532e950534b39029b
Yaron Levy <ron5054@gmail.com>
2024-06-15 18:00:23 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import { createServer } from 'http'
+
+const app = express()
+const server = createServer(app)
+
+const corsOptions = {
+  origin: ['http://127.0.0.1:5173', 'http://localhost:5174'],
+  credentials: true,
+}
+app.use(cors(corsOptions))
+
+app.get('/', (req, res) => {
+  res.send('Hello from Server!')
+})
+
+const port = 3030
+server.listen(port, () => console.log(`Server is running on port ${port}`))
