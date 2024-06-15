413d36eea61a6022d41d4a802d0be43e049942a3
Yaron Levy <ron5054@gmail.com>
2024-06-15 21:52:19 +0300

    first

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 import express from 'express'
 import cors from 'cors'
-import path from 'path'
 import { createServer } from 'http'
 
 const app = express()
 const server = createServer(app)
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve('public')))
-} else {
-    const corsOptions = {
-      origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
-      credentials: true,
-    }
-    app.use(cors(corsOptions))
-  }
-
+const corsOptions = {
+  origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+  credentials: true,
+}
+app.use(cors(corsOptions))
 
 app.get('/message', (req, res) => {
   res.send('Hello from Server!')
